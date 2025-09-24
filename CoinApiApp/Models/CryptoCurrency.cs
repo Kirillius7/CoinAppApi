3d9fe5c40546841829156ae3fcc8f0f54b6c4be9
Kyrylo Popov <popovkirill070320@gmail.com>
2025-09-24 19:36:12 +0300

    Створено клас CryptoCurrency

---
+++ CoinApiApp/Models/CryptoCurrency.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace CoinApiApp.Models
+{
+    public class CryptoCurrency
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+
+        [JsonPropertyName("current_price")]
+        public decimal CurrentPrice { get; set; }
+
+        [JsonPropertyName("market_cap")]
+        public decimal MarketCap { get; set; }
+
+        [JsonPropertyName("price_change_percentage_24h")]
+        public decimal PriceChangePercentage24h { get; set; }
+
+        // Нові поля
+        [JsonPropertyName("total_supply")]
+        public decimal TotalSupply { get; set; }            // Макс. кількість монет
+        [JsonPropertyName("circulating_supply")]
+        public decimal CirculatingSupply { get; set; }      // В обігу
+        [JsonPropertyName("total_volume")]
+        public decimal TotalVolume { get; set; }
+    }
+}
