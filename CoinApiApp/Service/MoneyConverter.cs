10d4177adc03056874456ad3a0ae060a60529bca
Kyrylo Popov <popovkirill070320@gmail.com>
2025-09-25 10:41:10 +0300

    Update MoneyConverter.cs

--- CoinApiApp/Service/MoneyConverter.cs
+++ CoinApiApp/Service/MoneyConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace CoinApiApp.Models
+namespace CoinApiApp.Service
 {
     public class MoneyConverter : IValueConverter
     {
