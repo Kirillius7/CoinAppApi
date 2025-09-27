5f014769c78328841fa6ed33f48653d42123fa12
Kyrylo Popov <popovkirill070320@gmail.com>
2025-09-27 10:37:30 +0300

    Додані коментарі до BaseViewModel

--- CoinApiApp/ViewModels/BaseViewModel.cs
+++ CoinApiApp/ViewModels/BaseViewModel.cs
@@ -8,10 +8,11 @@ using System.Threading.Tasks;
 
 namespace CoinApiApp.ViewModels
 {
-    public class BaseViewModel
+    public class BaseViewModel // клас для відправки повідомлення про зміни
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        // метод для виклику у set властивостях для зміни даних у UI
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
